f6eea8d5598f5cf885e28cfda3866caa0f590cc4
isaacngordon <45136480+isaacngordon@users.noreply.github.com>
2024-07-19 00:17:32 -0400

    Add a new 'destination' argument to the command line interface.

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ fn main() {
                         .arg(Arg::new("name").required(true))
                         .arg(Arg::new("file").short('f').long("file"))
                         .arg(Arg::new("dir").short('d').long("dir"))
-                )
+                        .arg(Arg::new("destination").short('d').long("destination"))
                 .subcommand(
                     Command::new("create")
                         .about("Creates a new template")
