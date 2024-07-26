fc4edf5fac946708d35eefb531084166169ae415
isaacngordon <45136480+isaacngordon@users.noreply.github.com>
2024-07-26 02:14:57 -0400

    Updated the Query enum to include additional variants for OAuth application, currencies, countries, provinces, businesses, user, and account types.

--- crates/waveapp/src/query.rs
+++ crates/waveapp/src/query.rs
@@ -2,24 +2,35 @@ use super::enums::{CountryCode, CurrencyCode};
 use super::scalar;
 
 pub enum Query {
+    /// Get the current OAuth application.
     OAuthApplication,
+    // List currencies
     Currencies,
+    // Get a currency
     Currency {
         code: CurrencyCode,
     },
+    // List countries
     Countries,
+    // Get a country
     Country {
         code: CountryCode,
     },
+    // Get a province
     Province(String),
+    // List businesses
     Businesses {
         page: scalar::Int,
         pageSize: scalar::Int,
     },
+    // Get a business
     Business {
         id: scalar::ID,
     },
+    // The currently authenticated user
     User,
+    // List ty[es of accounts]
     AccountTypes,
+    // List subtypes of accounts
     AccountSubTypes,
 }
