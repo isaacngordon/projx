447d061657bea7811cf9bcc6921ae6c5f4359249
isaacngordon <45136480+isaacngordon@users.noreply.github.com>
2024-08-09 18:12:41 -0400

    working on seeing build.rs actually run

--- crates/waveapp/build.rs
+++ crates/waveapp/build.rs
@@ -1,7 +1,9 @@
 fn main() {
+    println!("----------------------------\nStarting API discovery...\n----------------------------");
     cynic_codegen::register_schema("waveapp")
         .from_sdl_file("./schemas/wave_api_schema_20240728.graphql")
         .unwrap()
         .as_default()
         .unwrap();
+    println!("----------------------------\nAPI discovery complete.\n----------------------------");
 }
